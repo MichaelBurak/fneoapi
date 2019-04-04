63af6c6925256df562c6e714481ba0ceabb69001
MichaelBurak <michaelrburak@gmail.com>
2019-04-04 17:56:53 -0400

    index route grabs char and item from db

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,38 @@
-var express = require("express");
-var router = express.Router();
+const express = require("express");
+const router = express.Router();
+const neo4j = require("neo4j-driver").v1;
 
-/* GET home page. */
-router.get("/", function(req, res, next) {
-  res.render("index", { title: "Express" });
+const driver = neo4j.driver(
+  "bolt://localhost",
+  neo4j.auth.basic("neo4j", "pass")
+);
+
+const session = driver.session();
+
+// /* GET home page. */
+// router.get("/", function(req, res, next) {
+//   res.render("index", { title: "Express" });
+// });
+
+//Home Route
+router.get("/", function(req, res) {
+  session.run("MATCH (n:Character) RETURN n").then(function(result) {
+    var charArr = [];
+    result.records.forEach(function(record) {
+      charArr.push({
+        id: record._fields[0].identity.low,
+        name: record._fields[0].properties.name
+      });
+    });
+
+    session.run("MATCH (n:Item) RETURN n").then(function(resultItems) {
+      var itemArr = [];
+      resultItems.records.forEach(function(record) {
+        itemArr.push(record._fields[0].properties);
+      });
+      console.log(itemArr, charArr);
+    });
+  });
 });
 
 module.exports = router;
