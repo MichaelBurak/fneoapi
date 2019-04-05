8b241a6f2db3fe4848ec6c377b1e3bbf49426ab1
MichaelBurak <michaelrburak@gmail.com>
2019-04-05 10:34:20 -0400

    delete character route

--- routes/pcs/characters.js
+++ routes/pcs/characters.js
@@ -9,7 +9,7 @@ const driver = neo4j.driver(
 
 const session = driver.session();
 
-// Add Character Route
+// Add Character Route (C)
 
 router.post("/", function(req, res) {
   var name = req.body.name;
@@ -23,6 +23,19 @@ router.post("/", function(req, res) {
     })
     .catch(function(error) {
       console.log(error);
+    });
+});
+
+// Delete character route (D)
+
+router.delete("/delete/:character", function(req, res) {
+  var name = req.params.character;
+
+  // console.log(name);
+  session
+    .run("MATCH (n:Character {name:{name}}) DETACH DELETE n", { name })
+    .then(function(result) {
+      session.close();
     });
 });
 
